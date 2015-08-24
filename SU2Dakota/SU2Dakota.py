eec8cfbf0b61b851aebb8f461d0160ec34d5f592
padronas <padronas@stanford.edu>
2015-08-24 15:42:06 -0700

    Before making module

--- SU2Dakota/SU2Dakota.py
+++ SU2Dakota/SU2Dakota.py
@@ -1,19 +1,9 @@
 """This module allows to run SU2 from Dakota.
 
-   Run using SU2Dakota.run(**SU2_params).
-   #Maybe it doesn't have to be a keyword argument after all.
-# If all the arguments are specified.
-   
-   Required SU2_params.keys()
-     design_vars - list of design variables [0.01, 0.03,...]
-     uncertain_vars - dict of uncertain variables {'MACH_NUMBER': 0.8,...}
-     config - SU2 config class
+Run using SU2Dakota.run(config,eval_id,asv,x,u)
 
-  # Modules it uses list them
-  #Example - just copy an entire interface script when I have it
-
+This module makes use of the SU2 module.
 """ 
-
 
 import os, shutil
 import json
@@ -25,71 +15,92 @@ from record import Record
 import sys
 
 import SU2
-# I should be able to run UQ, Optimization and OUU.
 
-def run(**SU2_params):
-  '''Runs the problem '''
+def run(record_name,config,eval_id,asv,x=[],u={}):
+  """Run SU2. Return values specified in asv.
+
+  Args:
+    record_name (str): Filename of the record of simulations. JSON file.
+    config (config): A SU2 config class.
+    eval_id (int): The evaluation number.
+    asv ([int]): The active set vector, indicates desired simulator outputs.
+      For more information see Active Set Vector subsection in
+      Dakota user's manual.
+    x ([float]): design vector.
+    u ({}): uncertain variables. The KEY has to be a valid SU2 
+      configuration option {'MACH_NUMBER': 0.8,...}
   
-  x = SU2_params['design_vars'] # design vector
-  u = SU2_params['uncertain_vars']
-  asv = SU2_params['asv'] # Active set vector, indicates which simulator outputs are needed.
-  config = SU2_params['config']
-  i = SU2_params['eval_id']
-
-  link_mesh(config) 
-    
+  Returns:
+    returndict ({}): function, gradient, constrain values as specified in asv.
+  """
+  
   # Create a record to keep track of the simulations
-  #record_name = 'record.json'
-  record_name = '../record.json' # because running with folders do similar to config
+  record_name = '../' + record_name # because running dakota with folders
   if os.path.isfile(record_name):
     print 'Loading record of simulations'
     record = Record(record_name)
   else:
     print 'Creating new record of simulations'
     record = Record()
-  simulation = 'simulation' + str(i)
-  # Start populating the simulation in the record simulations
+  
+  # Initialize the simulation in the record simulations
+  simulation = 'simulation' + str(eval_id)
   record.simulations[simulation] = Bunch()
   record.simulations[simulation].design_vars = x
   record.simulations[simulation].uncertain_vars = u
-  record.nsimulations = i
+  record.simulations[simulation].directory = os.path.abspath('.')
+  record.nsimulations = eval_id
   
+  # Run the simulation
+  link_mesh(config) 
   returndict = {} 
    
-  if (asv[0] & 1): # **** f:
+  if (asv[0] & 1): # function
       f = func(record,config,x,u)
       returndict['fns'] = [f] # return list for now
       record.simulations[simulation].function = f
 
-  if (asv[0] & 2): # **** df/dx:
+  if (asv[0] & 2): # gradient function
       g = grad(record,config,x,u)
       returndict['fnGrads'] = [g] # return list for now
+      record.simulations[simulation].gradient = g
+  
+#  if (asv[1] & 1): # constrain
+#      f = cons(record,config,x,u)
+#      returndict['fns'] = [f] # return list for now
+#      record.simulations[simulation].function = f
 
-  # Populate record
-  record.simulations[simulation].directory = os.path.abspath('.')
+#  if (asv[1] & 2): # gradient constrain
+#      g = cons_grad(record,config,x,u)
+#      returndict['fnGrads'] = [g] # return list for now
+#      record.simulations[simulation].gradient = g
 
-  # Write out the record
+  # To add another constrain asv[2] and call the right the function.
+
+  
+  # Write out the record of simulations
   file = open(record_name,'w')
-  #print json.dumps(record, indent=2)
-  json.dump(record,file, indent=2)
+  #print json.dumps(record,indent=2)
+  json.dump(record,file,indent=2)
   file.close()
 
   return returndict
 
-# Need a little more work on this function
 def update_config(record,config,x,u):
-  '''sets up problem with the correct design and uncertain variables'''
+  '''Set up problem with the correct design and uncertain variables.
+  
+  Modifies the config to have the desired design and uncertain variables
+  values. If necessary it will perform a mesh deformation to account
+  for the updated design variables.
+  '''
 
-  # Check if it is a design problem
-  # Make this a function and have it in the gradient as well.
+  # Check for design problem
   if x:
     # Check if mesh deformation needed for the current design vector.
     if record.deform_needed(x):
       deform_mesh(config,x)
     
-  # unpack the uncertain variables
-  # Make this a function and have it in the gradient as well.
-  # Can call it set uncertain variables.
+  # Check for uq problem
   if u:
     for key in u.keys():
       config[key] = u[key]
@@ -101,7 +112,7 @@ def func_setup(folder_name,config):
   link_mesh(config)
 
 def restart2solution(config):
-  '''Moves restart file to solution file '''
+  '''Moves restart file to solution file.'''
   
   if config.MATH_PROBLEM == 'DIRECT':
     restart = config.RESTART_FLOW_FILENAME
@@ -119,13 +130,12 @@ def restart2solution(config):
   else:
     raise Exception, 'unknown math problem'
 
-
-def func(record,config,x=[],u={}):
+def func(record,config,x,u):
   
   print 'running (the function) direct problem ...'
   
   ### Pre-run ###
-  update_config(record,config,x,u) # explain what this does  
+  update_config(record,config,x,u) 
   folder_name = 'direct'
   config.MATH_PROBLEM = 'DIRECT'
   func_setup(folder_name,config)
@@ -164,7 +174,7 @@ def grad_setup(folder_name,config):
   dst = config.SOLUTION_FLOW_FILENAME
   shutil.copy(src,dst)
 
-def grad(record,config,x=[],u={}):
+def grad(record,config,x,u):
   
   print 'running (the gradient) adjoint problem ...'
   
@@ -213,7 +223,8 @@ def projection(config, step=1e-3):
 
 
 def deform_mesh(config,x):
- 
+  """Make a new mesh corresponding to design vector x."""
+  print 'deforming mesh ...'
   config.unpack_dvs(x)
   folder_name = 'deform'
   setup_folder(folder_name)
@@ -224,10 +235,8 @@ def deform_mesh(config,x):
   os.symlink(src,dst)
   
   log = 'log_Deform.out'
-  print 'deforming mesh ...'
   with SU2.io.redirect_output(log):
     SU2.run.DEF(config)
-  print 'finished deforming mesh.'
   
   # move updated mesh to correct location
   mesh_filename = config.MESH_FILENAME
@@ -237,18 +246,20 @@ def deform_mesh(config,x):
   shutil.move(src,dst)
   # return to the directory this function was called from
   os.chdir('..')
+  print 'finished deforming mesh.'
 
 def link_mesh(config):
   '''Links the mesh to the current working directory.'''
-
   mesh_filename = config.MESH_FILENAME
   src = '../' + mesh_filename
   dst = mesh_filename
   os.symlink(src,dst)
 
 def setup_folder(folder):
+  """Make folder and move into it."""
   # check, make folder
   if not os.path.exists(folder):
     os.makedirs(folder)
   # change directory
   os.chdir(folder)
+
--- SU2Dakota/SU2_dakota_interface.py
+++ SU2Dakota/SU2_dakota_interface.py
@@ -26,12 +26,9 @@ def main():
   # Setting up application
   # ----------------------
   
-  # -------------------------------
-  # Convert and send to application
-  # -------------------------------
-
   ########## Modify here for your problem ##########
   
+  record_name = 'record.json'
   ### Config file options ### 
   config_filename = 'inv_NACA0012_opt.cfg'
   # Create a config container
@@ -44,34 +41,29 @@ def main():
   #config.OPT_OBJECTIVE = 'DRAG' Maybe have this in the config file itself
   
   ### Dictionary for passing to your application (SU2) ### 
-  SU2_params = {}
-  SU2_params['config'] = config
-  SU2_params['uncertain_vars'] = {} # populate this below, problem specific
-  SU2_params['design_vars'] = [] # populate this below, problem specific
-  SU2_params['eval_id'] = int(paramsdict['eval_id'])
+  eval_id = int(paramsdict['eval_id'])
+  active_set_vector = [ int(paramsdict['ASV_1:obj_fn']) ]
+  active_set_vector.append(int(paramsdict['ASV_2:nln_ineq_con_1']) ] # maybe you don't have the one
   
   ### Modify the paramsdict names to match those of the params file ###
   
   # For information of the active set vector see
   # The Active Set Vector subsection of the Dakota user's manual
-  active_set_vector = [ int(paramsdict['ASV_1:obj_fn']) ]
-  SU2_params['asv'] = active_set_vector
   
-  # Specify uncertain variables, uncomment and modify as needed.
+  # Specify uncertain variables
   nu_var = 0
+  uncertain_vars = {}
   # the KEY has to be a valid SU2 configuration option
-  # SU2_params['uncertain_vars'][KEY] = ...
-  # SU2_params['uncertain_vars']['MACH_NUMBER'] = float(paramsdict['Mach'])
+  # uncertain_vars[KEY] = ...
+  # uncertain_vars['MACH_NUMBER'] = float(paramsdict['Mach'])
   
-  # Specify design variables, uncomment and modify as needed.
+  # Specify design variables
   nd_var = 38
+  design_vars = []
   for i in range(1,nd_var+1):
     var = 'x' + str(i)
-    SU2_params['design_vars'].append(float(paramsdict[var]))
+    design_vars.append(float(paramsdict[var]))
   
-  print SU2_params['design_vars']
-  print len(SU2_params['design_vars'])
-  #SU2_params['functions'] = ['LIFT']
   
   # rough error checking
   nvar = nd_var + nu_var
@@ -88,13 +80,13 @@ def main():
   num_fns = 0
   if ('functions' in paramsdict):
       num_fns = int(paramsdict['functions'])
-  SU2_params['nfunctions'] = num_fns
   
 
   # execute the SU2 analysis
   import SU2Dakota
   print "Running SU2..."
-  resultsdict = SU2Dakota.run(**SU2_params)
+  resultsdict = SU2Dakota.run(record_name,config,
+                eval_id,active_set_vector,design_vars,uncertain_vars)
   print "SU2 complete."
   print resultsdict
 
